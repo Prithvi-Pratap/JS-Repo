0ba95990cd95ab406cb6480788b3c0bce965f8c6
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2023-11-12 22:21:06 +0000

    pushing the string conversion file

---
+++ 01_basics/02_stringToNumberConversion.js
@@ -0,0 +1,34 @@
+// let value=3;
+// let negValue=-value;
+// console.log(negValue);
+
+//BASIC OPERATIONS
+// console.log(4+4)
+// console.log(4-4)
+// console.log(4*4)
+// console.log(4**3) //4 raise to the power of 3
+// console.log(4/4)
+// console.log(4%4)
+
+//operaion with strings
+
+// let str1="Prithvi";
+// let str2="Pratap";
+// console.log(str1+" "+str2)
+
+// console.log(1+"2") //Addition of any number to string results in concatination
+// console.log("1"+2)
+// console.log("1"+2+3)
+// console.log(1+2+"3") //operation starts from left and end up being concatinated coz of addition operator with string
+// console.log(1+"2"+3)
+
+// console.log(true)
+// console.log(+true) //unary + ensures that the oparend is treated as a number
+// console.log(+false)
+// console.log(-true)//-1
+// console.log(-"7")
+// console.log(-"-7")
+// console.log(-false)//-0
+// console.log(typeof(-false))//number
+
+// the unary plus is applied to the variable a, and although it doesn't change the sign of a, it ensures that b is treated as a numeric value. It's worth noting that in most cases, the unary plus is not explicitly needed, as JavaScript will automatically convert values to numbers in many contexts. However, using the unary plus can be useful in cases where you want to ensure that a value is treated as a number, especially when dealing with user input or other dynamic data sources.
