414d7fa3dd27174935e9161b7a4cd1df86f3a98f
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2023-11-13 02:10:20 +0000

    lesson 04 file added

---
+++ 01_basics/04_MoreOnDataTypes.js
@@ -0,0 +1,19 @@
+//on the basis of storing and accessing the variables, data types are categorised in two types:
+//A. Primitive Data Type:there are 7 primitive data types
+
+    //1.number
+    //2.null
+    //3.symbol
+    //4.string
+    //5.boolean
+    //6.bigInt
+    //7.undefined
+
+//B.Non-primitive(reference type): Array, object, function
+let arr=["honey","prithvi","pratap"]; //array
+ 
+{
+type:"Fiat"; 
+model:"500";
+color:"white"
+};
