61effbad7008302a3c4e50ec69165e5aaba57a01
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2023-12-17 09:30:01 +0000

    update

--- 02_basics/temp-promises.js
+++ 02_basics/temp-promises.js
@@ -1,4 +1,4 @@
-const myPromise= new Promise((resolve,reject)=>{
+/*const myPromise= new Promise((resolve,reject)=>{
     setTimeout(function(){
         console.log("Inside the promise")
         resolve({
@@ -12,5 +12,65 @@ const myPromise= new Promise((resolve,reject)=>{
 myPromise.then((user)=>{
     console.log(user.email)
     console.log(user.username)
-    console.log(user)
+    // console.log(user)
 })
+
+//promise: handle the rejection;
+
+const myPromise2=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        let error=true;
+        if(!error){
+            resolve(
+                {username:"Prithvi-Pratap",email:"pratapprithvi768@gmail.com",phoneNo:7667025535}
+                )
+            }
+            else{
+                reject('ERROR:Something went wrong!');
+            }
+        },1000)
+    })
+    
+    myPromise2.then((user)=>{
+        console.log(user)
+    }).catch((error)=>{
+        console.log(error);
+    })
+    
+    */
+
+//chaining in the promises:
+
+const myPromise3 = new Promise((resolve, reject) => {
+    let error = false;
+    if (!error) {
+        resolve(
+            {
+                username: "chai aur  code",
+                email: "chai@chaiaurcode.com",
+                password: '123@chai'
+            }
+        )
+    }
+    else {
+        reject('ERROR:Something went wrong!');
+    }
+})
+
+//chaining starts here
+
+myPromise3
+    .then((user) => {
+        //this return statement returns a object containing the following properties
+        return ({username:user.username, password:user.password})
+    })
+    //return value of above then() goes as the argument of the below function
+    .then((userData) => {
+        console.log(`Username: ${userData.username}\nPassword: ${userData.password}`)
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+    .finally(() => {
+        console.log("The Promise is either Resolved or Rejected!");
+    })
